1d8d71b6c60437e6cea8d24391ee7362990c58d2
Pedro <pedro.pires@sptech.school>
2023-12-07 20:07:41 -0300

    feat: adicionando rotas e imagens à pasta 'assets'.

---
+++ src/themes/themes.ts
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+    'black': '#000',
+    'blue-100': '#8ECAE6',
+    'blue-200': '#1195FF',
+    'blue-600': '#0B215C',
+    'blue-800': '#0A142F',
+    'gray-100': '#F8F9FA',
+    'gray-200': '#DEDEDE',
+    'gray-300': '#979797',
+    'gray-400': '#555555',
+    'gray-800': '#2E3436',
+    'gray-900': '#292929',
+    'green': '#38B000',
+    'red': '#D90429',
+    'white': '#FFF',
+    'yelloe-100': '#FFF7EB'
+}
+  
